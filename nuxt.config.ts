f0fee668086cb6d169ea37a21fb303198e991f3b
Daria Komarenko <dkomarenko@spherical.pm>
2025-04-28 10:43:05 +0300

    Initial commit: nuxt, tailwind configs setup

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    scanPageMeta: 'after-resolve',
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    spaLoadingTemplateLocation: 'within',
+    parseErrorData: false,
+    pendingWhenIdle: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/test-utils',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/google-fonts',
+    '@nuxt/icon'
+  ],
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+})
