40b49c5be6996ea833ee17421bcb167555f86b15
Daria Komarenko <dkomarenko@spherical.pm>
2025-04-28 16:00:43 +0300

    Add statis images to images folder

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -4,7 +4,7 @@
   <header class="py-4 border-b">
     <nav class="flex container items-center">
       <NuxtLink to="/" class="flex gap-1 items-center">
-        <NuxtImg width="56px" densities="x1" format="webp" src="/icon-green.png" alt="" />
+        <NuxtImg width="56px" densities="x1" format="webp" src="/images/icon-green.png" alt="" />
         <span class="text-2xl font-bold">Nuxt Recipes</span>
       </NuxtLink>
       <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
